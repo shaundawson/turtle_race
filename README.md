a5c5f54b344f34996617491611c08658ea3075d7
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-16 18:20:54 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # turtle_race
- 
+ Bet on the color of a turtle who will win the race
+ Turtles move at a random pace
+ Once the first turle reaches the right edge of the screen, winner is declared
+
+
+RESOURCES:
+https://docs.python.org/3/library/turtle.htm
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import turtle as turtle_module
+from turtle import Turtle, Screen
 
-
-screen = turtle_module.Screen()
+screen = Screen()
 screen.setup(width=500,height=400)
 
 user_bet = screen.textinput(title ="Make your bet", prompt = "Which turtle will win the race? Enter a color:")
+user_bet = screen.textinput(title ="Make your bet", prompt = "Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_position = [-70, -40, -10, 20, 50, 80]
 
-tim = turtle_module.Turtle(shape="turtle")
-tim.penup
-
-
-#Take turtles to starting line
-tim.goto(x=-230,y=100)
+for turtle_index in range (0,6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[turtle_index])
+    tim.penup
+    #Take turtles to starting line
+    tim.goto(x=-230,y=y_position[turtle_index])
 
 screen.exitonclick()
