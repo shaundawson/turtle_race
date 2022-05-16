6f7f4a031aadb8e4c88a616b442d7cf2fde9c89a
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-16 18:21:06 -0400

    update

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=500,height=400)
 
-user_bet = screen.textinput(title ="Make your bet", prompt = "Which turtle will win the race? Enter a color:")
 user_bet = screen.textinput(title ="Make your bet", prompt = "Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_position = [-70, -40, -10, 20, 50, 80]
