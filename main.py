0e2382726c6a459574967374c094a4e45414e365
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-16 18:22:01 -0400

    remove lines

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ y_position = [-70, -40, -10, 20, 50, 80]
 for turtle_index in range (0,6):
     tim = Turtle(shape="turtle")
     tim.color(colors[turtle_index])
-    tim.penup
+    tim.penup()
     #Take turtles to starting line
     tim.goto(x=-230,y=y_position[turtle_index])
 
