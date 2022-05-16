5383a4f86c200e7243c9ffe7e6358da2f0386988
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-16 17:56:34 -0400

    change shape

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import turtle as turtle_module
 
-tim = turtle_module.Turtle()
+
 screen = turtle_module.Screen()
 screen.setup(width=500,height=400)
 
 user_bet = screen.textinput(title ="Make your bet", prompt = "Which turtle will win the race? Enter a color:")
 
+tim = turtle_module.Turtle(shape="turtle")
+tim.penup
+
+
+#Take turtles to starting line
+tim.goto(x=-230,y=100)
 
 screen.exitonclick()
